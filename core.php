0d3e6412178afb660a04938eac29708bfe0fcde0
Marcelo Serra <marcelo.serra@fatec.sp.gov.br>
2020-07-18 23:44:22 -0300

    Object Oriented applied

--- core.php
+++ core.php
@@ -1,30 +1,9 @@
 <?
 
 
-$links = json_decode($_POST['links']);
-$dir = './processed/';
+include 'src/download-class.php';
 
-
-function crawlUrl($url){
-    $ch = curl_init($url);
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER,true );
-    curl_setopt( $ch, CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT'] );
-    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true ); 
-    curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
-    $res=curl_exec( $ch );
-    curl_close( $ch );
-    return $res;
-}
-
-foreach($links as $link){
-
-    $fileName = basename($link);
-    if(file_put_contents($dir.$fileName, crawlUrl($link))){
-        echo 'Downloaded: '.$link.'<br>';
-    }else{
-        echo 'Failed: '.$link.'<br>';
-    }
-
-}
+$downloader = new Download(json_decode($_POST['links']), './processed/');
+$downloader->iteratorLinks();
 
 ?>
---
+++ src/download-class.php
@@ -0,0 +1,58 @@
+<?
+
+class Download{
+    private $links;
+    private $dir;
+
+    public function __construct($arrayUrls, $stringDir){
+        $this->links = $arrayUrls;
+        $this->dir = $stringDir;
+    }
+
+    public function setLinks($arrayLinks){
+        foreach($arrayUrls as $this->links => $value){
+            $this->links = $value;
+        }
+    }
+
+    public function getLinks(){
+        return $this->links;
+      }
+
+    public function setDir($stringDir){
+        $this->dir = $stringDir;
+    }
+
+    public function getDir(){
+        return $this->dir;
+    }
+
+
+    private function fetchLink($url){
+        $ch = curl_init($url);
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER,true );
+        curl_setopt( $ch, CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT'] );
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true ); 
+        curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
+        $res=curl_exec( $ch );
+        curl_close( $ch );
+        return $res;
+    }
+
+    public function iteratorLinks(){
+
+        foreach($this->links as $link){
+            $fileName = basename($link);
+            if(file_put_contents($this->dir.$fileName, $this->fetchLink($link))){
+                echo 'Downloaded: '.$link.'<br>';
+            }else{
+                echo 'Failed: '.$link.'<br>';
+            }
+        
+        }
+        
+      }
+}
+
+
+?>
