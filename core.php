bd763cb03f88bd47f9b0672ab9f5b6d11df5edf1
Marcelo Serra <marcelo.serra@fatec.sp.gov.br>
2020-07-19 00:09:13 -0300

    process folder renamed to downloaded

--- core.php
+++ core.php
@@ -2,7 +2,7 @@
 
 
 include 'src/download-class.php';
-$downloader = new Download(json_decode($_POST['links']), './processed/');
+$downloader = new Download(json_decode($_POST['links']), './downloaded/');
 $downloader->iteratorLinks();
 //$downloader->getLinks();
 //$downloader->getDir();
